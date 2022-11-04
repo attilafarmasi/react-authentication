90eca341ed670ea60143dedeaf883a9f9bc72dc2
Farmasi Attila <attila.farmasi@dpdgroup.com>
2022-11-04 15:54:21 +0100

    Redirecting the User

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,9 +1,12 @@
 import { useRef, useContext } from "react";
+import { useHistory } from "react-router-dom";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const history = useHistory();
+
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -25,10 +28,10 @@ const ProfileForm = () => {
         }),
         headers: { "Content-Type": "application/json" },
       }
-    ).then(res => {
+    ).then((res) => {
       // assumption: Always succeeds!
-
       
+      history.replace("/");
     });
   };
 
